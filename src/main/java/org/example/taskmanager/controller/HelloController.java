4a941c63c1c998470f848c350c9ac3b50b73ac0d
furkanatlan <furkanatlann@gmail.com>
2026-04-15 11:57:06 +0300

    First Controller and testing endpoint

---
+++ src/main/java/org/example/taskmanager/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Merhaba, Spring Boot calisiyor!";
+    }
+}
